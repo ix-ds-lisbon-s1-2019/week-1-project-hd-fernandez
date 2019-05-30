76b253d6eb4faa1e61712c5cd5a50cfce2fdd239
hdfernandez <hdfernandez@me.com>
2019-05-30 17:06:58 +0100

    added .py file

---
+++ poker_game.py
@@ -0,0 +1,18 @@
+# Henry Fernandez
+# Poker Game
+
+print ('Number of players?')
+number_of_players = input()
+
+players = []
+
+for i in range(len(number_of_players)):
+	print('Enter name of player' + i)
+	temp_player = input()
+	temp_player = players.append(i)
+
+print(players)
+
+
+
+
