5079f2c7642b87776b3d31f708ab3db36950047d
hdfernandez <hdfernandez@me.com>
2019-05-31 11:03:56 +0100

    FINALLY

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Here is the repo with the project brief and where you should push your final poker game .py file. Might be a good idea to edit this README to include a link to your blog (where in a post you should link this repo too) :) 
 
 The deadline is **tomorrow (Friday) the 31st of May at 8pm**.
+
+
+The poker game begins by prompting the user to input how many players will participate in the game. It then runs a loop that asks the user to enter the names of the players and adds these names to a list. A ard class is created that specify the card's rank and suit. Another __repr__ method helps format how the cards are presented. After a deck is created and cards are assigned to users, the game runs and the player with the highest value card wins.
--- poker_game.py
+++ poker_game.py
@@ -3,15 +3,74 @@
 
 print ('Number of players?')
 number_of_players = input()
+number_of_players = int(number_of_players)
 
 players = []
 
-for i in range(len(number_of_players)):
-	print('Enter name of player' + i)
+for i in range(1, number_of_players + 1):
+	print('Enter name of player ('+ str(i) + ')')
 	temp_player = input()
-	temp_player = players.append(i)
+	players.append(temp_player)
 
+print('')
 print(players)
+
+class Card(object):
+    
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+    
+    def __repr__(self):
+        return str(self.rank) + ' of ' + str(self.suit)
+    
+    VALUE = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J','Q','K','A']
+    SUIT = ["Hearts","Clubs","Diamonds","Spades"]
+    
+    def __str__ (self):
+        if self.rank == 'J':
+            rank = 11
+        elif self.rank == 'Q':
+            rank = 12
+        elif self.rank == 'K':
+            rank = 13
+        elif self.rank == 'A':
+            rank = 14
+        else:
+            rank = self.rank
+           # __repr__(self)
+    
+
+#card=Card('Q', 'Diamonds')
+#print(card)
+deck = []
+
+player_hands = {}
+
+def make_deck():
+    for suit in Card.SUITS:
+      for rank in Card.RANKS:
+        card = Card (rank, suit)
+        deck.append(card)
+
+make_deck()
+
+def deal_deck():
+    for i in players:
+        for j in deck:
+            player_hands = 
+    
+        
+
+
+        
+        
+        
+        
+
+
+    
+    
 
 
 
